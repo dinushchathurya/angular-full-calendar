811da6966710f0dbeb927118ac8dfdf4b2c40077
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-16 15:02:29 +0530

    :sparkles: Peering full-calendar to application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction';
+import { AddEventComponent } from './pages/calendar/add-event/add-event.component';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FullCalendarModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
