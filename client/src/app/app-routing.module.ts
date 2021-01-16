089679561010d709318fb3609abb784d0a996609
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-16 15:01:50 +0530

    :sparkles: Create routes path to calendar and add-event components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { AddEventComponent } from './pages/calendar/add-event/add-event.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'add-event', component: AddEventComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
