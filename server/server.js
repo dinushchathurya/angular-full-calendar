c87689fa2bc7984e2d1a3d431b2f8340ea1f23ba
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-16 10:11:12 +0530

    :sparkles: Create server and configure routes paths

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import eventRoutes from './routes/events.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/events', eventRoutes);
+
+const CONNECTION_URL = '<your-connection-string>';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
+
